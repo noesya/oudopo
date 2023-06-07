a380c983c103008efc06ed28b7e1e28379fcacbe
Arnaud Levy <contact@arnaudlevy.com>
2023-06-07 11:01:10 +0200

    annotate

--- test/models/archive_test.rb
+++ test/models/archive_test.rb
@@ -1,3 +1,24 @@
+# == Schema Information
+#
+# Table name: archives
+#
+#  id              :uuid             not null, primary key
+#  description     :text
+#  name            :string
+#  secret          :string
+#  url             :string
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#  organisation_id :uuid             not null, indexed
+#
+# Indexes
+#
+#  index_archives_on_organisation_id  (organisation_id)
+#
+# Foreign Keys
+#
+#  fk_rails_71b4056303  (organisation_id => organisations.id)
+#
 require "test_helper"
 
 class ArchiveTest < ActiveSupport::TestCase
--- test/models/organisation_test.rb
+++ test/models/organisation_test.rb
@@ -1,3 +1,14 @@
+# == Schema Information
+#
+# Table name: organisations
+#
+#  id         :uuid             not null, primary key
+#  name       :string
+#  slug       :string
+#  website    :string
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
 require "test_helper"
 
 class OrganisationTest < ActiveSupport::TestCase
