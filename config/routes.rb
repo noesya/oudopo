3f617e4befb1517ba6407906ed6036fc300f5bae
Arnaud Levy <contact@arnaudlevy.com>
2023-06-07 07:41:26 +0200

    wp

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,4 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
+  get 'wordpress' => "pages#wordpress"
   root "pages#index"
 end
