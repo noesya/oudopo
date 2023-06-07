289f7a619e829e79f722fc38dacb876be064f98f
Arnaud Levy <contact@arnaudlevy.com>
2023-06-07 08:33:38 +0200

    devise

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,11 @@ module Oudopo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.sass.preferred_syntax = :sass
+
+    config.i18n.available_locales = [:fr]
+    config.i18n.default_locale = :fr
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
